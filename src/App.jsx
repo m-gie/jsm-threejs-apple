c51315f31a9a75df46107547ce9940e4449e52bb
m.glowacki <m.glowacki@smsnet.pl>
2024-07-08 15:53:54 +0200

    Add partial Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
+      <Hero />
+      <Highlights />
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { appleImg, searchImg, bagImg } from "../utils";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple" width={18} height={18} />
+        <div>
+          {["Phones", "MacBooks", "Tables"].map((item, index) => (
+            <div key={nav}>{item}</div>
+          ))}
+        </div>
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
